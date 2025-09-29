4dc0decfd5131ec6e6f5be90aedf54a8a0c2c994
Miguel Ernandes <152460710+MIGUELEDL@users.noreply.github.com>
2025-09-29 17:53:38 -0300

    Versão 001
    primeira versão

---
+++ Páginas/script2.js
@@ -0,0 +1,9 @@
+function toggleSection(element) {
+    const content = element.nextElementSibling;
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}

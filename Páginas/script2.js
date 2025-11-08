47b4bd5b8e971abaf602fdb4c85b2eff5a59096b
Miguel Ernandes <152460710+MIGUELEDL@users.noreply.github.com>
2025-11-08 04:09:21 -0300

    Delete Páginas directory

--- Páginas/script2.js
+++
@@ -1,9 +0,0 @@
-function toggleSection(element) {
-    const content = element.nextElementSibling;
-
-    if (content.style.display === "block") {
-        content.style.display = "none";
-    } else {
-        content.style.display = "block";
-    }
-}
